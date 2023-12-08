379c6e827964e3b571ee66cacc4bd396f05061cb
devopseasylearning <info@devopseasylearning.com>
2023-12-08 15:50:48 -0500

    updated

--- weatherapp/auth/src/authdb/authdb.go
+++ weatherapp/auth/src/authdb/authdb.go
@@ -5,63 +5,60 @@ import (
 	"database/sql"
 	"encoding/hex"
 	"fmt"
-	"log"
 )
 
 type User struct {
-	ID       int    `json:"user_id"`
+	ID       int    `json: "user_id"`
 	Name     string `json:"user_name"`
 	Password string `json:"user_password"`
 }
 
-func Connect(dbRoot string, dbPassword string, dbHost string) (*sql.DB, error) {
+func Connect(dbRoot string, dbPassword string, dbHost string) *sql.DB {
 	db, err := sql.Open("mysql", fmt.Sprintf("%s:%s@tcp(%s:3306)/", dbRoot, dbPassword, dbHost))
 	if err != nil {
-		return nil, err
+		fmt.Println(err.Error())
 	}
-
-	if err = db.Ping(); err != nil {
-		return nil, err
-	}
-
-	return db, nil
+	return db
 }
 
-func CreateDB(db *sql.DB) error {
-	_, err := db.Exec("CREATE DATABASE IF NOT EXISTS auth")
+func CreateDB(db *sql.DB) {
+	cmd, err := db.Query("CREATE DATABASE IF NOT EXISTS auth")
 	if err != nil {
-		return err
+		fmt.Println(err.Error())
 	}
-	return nil
+	defer cmd.Close()
 }
-
-func CreateTables(db *sql.DB) error {
-	_, err := db.Exec("CREATE TABLE IF NOT EXISTS auth.users (user_id int AUTO_INCREMENT, user_name char(50) NOT NULL, user_password char(128), PRIMARY KEY(user_id));")
+func CreateTables(db *sql.DB) {
+	cmd, err := db.Query("CREATE TABLE IF NOT EXISTS auth.users (user_id int AUTO_INCREMENT,  user_name char(50) NOT NULL, user_password char(128), PRIMARY KEY(user_id));")
 	if err != nil {
-		return err
+		fmt.Println(err.Error())
 	}
-	return nil
+	defer cmd.Close()
 }
-
 func InsertUser(db *sql.DB, user User) error {
 	password := md5.Sum([]byte(user.Password))
-	_, err := db.Exec("INSERT INTO auth.users (user_name,user_password) VALUES (?, ?)", user.Name, hex.EncodeToString(password[:]))
+	cmd, err := db.Query(fmt.Sprintf("INSERT INTO auth.users (user_name,user_password) VALUES ('%s','%s');", user.Name, hex.EncodeToString(password[:])))
 	if err != nil {
 		return err
 	}
+	defer cmd.Close()
 	return nil
 }
-
-func GetUserByName(userName string, db *sql.DB) (User, error) {
+func GetUserByName(user_name string, db *sql.DB) (User, error) {
 	var user User
-	row := db.QueryRow("SELECT * FROM auth.users WHERE user_name = ?", userName)
-	err := row.Scan(&user.ID, &user.Name, &user.Password)
+	results, err := db.Query(fmt.Sprintf("SELECT * FROM auth.users where user_name = '%s'", user_name))
 	if err != nil {
 		return user, err
 	}
+	defer results.Close()
+	for results.Next() {
+		err = results.Scan(&user.ID, &user.Name, &user.Password)
+		if err != nil {
+			return user, err
+		}
+	}
 	return user, nil
 }
-
 func CreateUser(db *sql.DB, u User) (bool, error) {
 	user, err := GetUserByName(u.Name, db)
 	if err != nil {
@@ -69,12 +66,12 @@ func CreateUser(db *sql.DB, u User) (bool, error) {
 	}
 	if user != (User{}) {
 		return false, nil
+	} else {
+		err := InsertUser(db, u)
+		if err != nil {
+			return false, err
+		} else {
+			return true, nil
+		}
 	}
-
-	err = InsertUser(db, u)
-	if err != nil {
-		return false, err
-	}
-
-	return true, nil
 }
