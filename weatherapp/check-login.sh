748f33f81802203ff948cd8880fa43491b12415b
devopseasylearning <info@devopseasylearning.com>
2023-12-13 15:45:54 -0500

    updated

--- weatherapp/check-login.sh
+++ weatherapp/check-login.sh
@@ -1,11 +1,22 @@
 #!/bin/bash
 ip_address=$(curl -s https://api.ipify.org ; echo)
 ip_port=3000
-curl http://$ip_address:$ip_port/login | grep '<title>Weather app login</title>'
+url="http://$ip_address:$ip_port/login"
+expected_title='<title>Weather app login</title>'
 
-if 
-  [[  $? -ne 0 ]] 
-then 
-echo "The website is not comming up, please check"
-exit 1
+response=$(curl -s -o /dev/null -w "%{http_code}" "$url")  # Check response code
+
+if [[ $response -ne 200 ]]; then
+    echo "The website is not responding (HTTP status code: $response), please check"
+    exit 1
+fi
+
+title_check=$(curl -s "$url" | grep -c "$expected_title")
+
+if [[ $title_check -eq 0 ]]; then
+    echo "The expected title '$expected_title' was not found. The website might be down or changed its structure."
+    exit 1
+else
+    echo "The website is up and the expected title '$expected_title' was found."
+    exit 0
 fi
