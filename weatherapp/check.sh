e86d0fe2c4412945b2624ec989a956e406d1b7b6
devopseasylearning <info@devopseasylearning.com>
2023-12-15 02:35:04 -0500

    updated

--- weatherapp/check.sh
+++ weatherapp/check.sh
@@ -1,29 +1,14 @@
 #!/bin/bash
 
-# Array of container names
-containers=('weatherapp-auth' 'weatherapp-db' 'weatherapp-redis' 'weatherapp-ui' 'weatherapp-weather')
-
-# Flag to track if any container is not running
-containers_not_running=0
-
-for container in "${containers[@]}"
+for i  in 'weatherapp-auth' 'weatherapp-db' 'weatherapp-redis' 'weatherapp-ui' 'weatherapp-weather'
 do 
-    # Check if the container is running
-    RUNNING=$(docker-compose ps | grep "$container" | grep -o Up)
+RUNNING=$(docker-compose ps | grep $i | grep -o Up)
 
-    if [[ ${RUNNING} == Up ]]
-    then 
-        echo "$container container is in running state"
-    else 
-        echo "$container container is NOT in running state"
-        containers_not_running=1  # Set the flag if any container is not running
-    fi
-done
-
-# Check the flag to determine the overall status
-if [ $containers_not_running -eq 1 ]
-then
-    exit 1  # Exit with status 1 if any container is not running
-else
-    exit 0  # Exit with status 0 if all containers are running
+if [[ ${RUNNING} == Up ]]
+  then 
+  echo "$i container is in running state"
+  else 
+  echo "$i container is NOT in running state"
+  exit 1
 fi
+done
